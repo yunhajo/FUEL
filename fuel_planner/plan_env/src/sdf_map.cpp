458c93ca7fc031b0091d019567f5b1911b19d80a
yunhajo <yjo@caltech.edu>
2024-08-09 12:59:18 -0700

    Changed launch file to get rid of the trigger

--- fuel_planner/plan_env/src/sdf_map.cpp
+++ fuel_planner/plan_env/src/sdf_map.cpp
@@ -31,6 +31,7 @@ void SDFMap::initMap(ros::NodeHandle& nh) {
   mp_->local_bound_inflate_ = max(mp_->resolution_, mp_->local_bound_inflate_);
   mp_->resolution_inv_ = 1 / mp_->resolution_;
   mp_->map_origin_ = Eigen::Vector3d(-x_size / 2.0, -y_size / 2.0, mp_->ground_height_);
+  // mp_->map_origin_ = Eigen::Vector3d(0.0, 0.0, 0.0);
   mp_->map_size_ = Eigen::Vector3d(x_size, y_size, z_size);
   for (int i = 0; i < 3; ++i)
     mp_->map_voxel_num_(i) = ceil(mp_->map_size_(i) / mp_->resolution_);
--- uav_simulator/Utils/waypoint_generator/src/waypoint_generator.cpp
+++ uav_simulator/Utils/waypoint_generator/src/waypoint_generator.cpp
@@ -153,7 +153,7 @@ void goal_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
   // ROS_ASSERT(trigged_time > ros::Time(0));
 
   ros::NodeHandle n("~");
-  n.param("waypoint_type", waypoint_type, string("manual"));
+  n.param("waypoint_type", waypoint_type, string("point"));
 
   if (waypoint_type == string("circle")) {
     waypoints = circle();
