824d6c9644cfd59a39df5be2dcf94f97855144a4
yunhajo <yunhajo03@gmail.com>
2025-04-08 16:18:43 -0700

    swtiched order of includes

--- fuel_planner/plan_env/src/sdf_map.cpp
+++ fuel_planner/plan_env/src/sdf_map.cpp
@@ -1,8 +1,8 @@
 #include "plan_env/sdf_map.h"
 #include "plan_env/map_ros.h"
 #include <plan_env/raycast.h>
-#include "plan_env/box.h"
 #include "ros/ros.h"
+#include "plan_env/box.h"
 
 namespace fast_planner {
 SDFMap::SDFMap() {
