455ba268658ce8dbe2cf07159cd443935c8e3d1e
yunhajo <yunhajo03@gmail.com>
2025-04-08 18:04:55 -0700

    commented out include line

--- fuel_planner/plan_env/src/sdf_map.cpp
+++ fuel_planner/plan_env/src/sdf_map.cpp
@@ -2,7 +2,7 @@
 #include "plan_env/map_ros.h"
 #include <plan_env/raycast.h>
 #include "ros/ros.h"
-#include "plan_env/box.h"
+// #include <plan_env/box.h>
 
 namespace fast_planner {
 SDFMap::SDFMap() {
