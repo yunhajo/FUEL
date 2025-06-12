fab81b9854d851872de3d85d755f168faec5bdcd
yunhajo <yunhajo03@gmail.com>
2025-06-12 12:09:26 -0700

    fixed error in header file

--- fuel_planner/exploration_manager/include/exploration_manager/fast_exploration_fsm.h
+++ fuel_planner/exploration_manager/include/exploration_manager/fast_exploration_fsm.h
@@ -61,7 +61,7 @@ private:
   void frontierCallback(const ros::TimerEvent& e);
   void triggerCallback(const nav_msgs::PathConstPtr& msg);
   void odometryCallback(const nav_msgs::OdometryConstPtr& msg);
-  void bboxCallback(const geometry_msgs::PoseArray::ConstPtr& msg, ros::NodeHandle& nh);
+  void bboxCallback(const geometry_msgs::PoseArray::ConstPtr& msg);
   void visualize();
   void clearVisMarker();
 
